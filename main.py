47e074d3644fe48d5418744a35766d469fe0f43f
AlexanderK011 <kurasov_04@list.ru>
2023-09-04 21:00:21 +0300

    на четных числах ставится строка strk

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ chislo = int(input('Введите число: '))
 strk=input('Введите строку: ')
 mass=[]
 for i in range(0,chislo):
-    mass.append(i)
+    if i%2 == 0:
+        i=strk
+        mass.append(strk)
+    else:
+        mass.append(i)
